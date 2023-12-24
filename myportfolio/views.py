a80ddafb602cfd936f6abdf4a2893e62edac6d11
simran <simrankumaribodhgaya93@gmail.com>
2023-12-24 20:02:03 +0530

    settins changed

--- myportfolio/views.py
+++ myportfolio/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.Http import  HttpResponse
+from . models import Feature 
 # Create your views here.
+
+def index(request):
+    features = Feature.objects.all()
+    feature1 = Feature
+    feature1.id
+    return render(request, 'index.html', {'features': features})
